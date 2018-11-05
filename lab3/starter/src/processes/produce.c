647d8072cd63a124bc657804af7ff5dd22150f9c
Zahin Mohammad <Zahin.Mohammad@gmail.com>
2018-11-05 18:36:10 -0500

    Finished proccesses

--- lab3/starter/src/processes/produce.c
+++ lab3/starter/src/processes/produce.c
@@ -25,27 +25,25 @@ void doProducerWork();
 void doConsumerWork();
 
 //message queue's
-
-
 mqd_t qdes;
-char *qname = "/mailbox";
+char *qname = "/mailbox10";
 
-mqd_t pdes;
-char *pname = "/pmailbox";
+
 
 int pid;
 int producer_id = 0;
 int consumer_id = 0;
-
-int main(int argc, char *argv[])
-{
-	int num;
+struct timeval tv;
+int num;
 	int maxmsg;
 	int num_p;
 	int num_c;
-	int i;
-	struct timeval tv;
+	
 
+int main(int argc, char *argv[])
+{
+	
+	//int i;
 	if (argc != 5) {
 		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
 		exit(1);
@@ -62,41 +60,27 @@ int main(int argc, char *argv[])
 	printTime();
 		
 	
-	
-	
 	//message queue setup. Each child process will see the same message box
-	char  quit = '\0';
+	//Produers will send to qdes and consumers will consume form qdes
 	mode_t mode = S_IRUSR | S_IWUSR;
 	struct mq_attr attr;
 
 	attr.mq_maxmsg  = maxmsg;
-	attr.mq_msgsize = sizeof(struct point);
+	attr.mq_msgsize = sizeof(int);
 	attr.mq_flags   = 0;		/* a blocking queue  */
 	
 	qdes  = mq_open(qname, O_RDWR | O_CREAT, mode, &attr);
 	if (qdes == -1 ) {
-		perror("mq_open() failed");
+		printf("mq_open() failed");
 		exit(1);
 	}
 	
 	//message queue setup for producers. Each producer process will see the number of items left to produce.
-	char  pquit = '\0';
-	mode_t pmode = S_IRUSR | S_IWUSR;
-	struct mq_attr pattr;
-
-	pattr.mq_maxmsg  = 1;
-	pattr.mq_msgsize = sizeof(int);
-	pattr.mq_flags   = 0;		/* a blocking queue  */
 	
-	pdes  = mq_open(pname, O_RDWR | O_CREAT, pmode, &pattr);
-	if (pdes == -1 ) {
-		perror("mq_open() failed");
-		exit(1);
-	}
+	attr.mq_maxmsg  = 1;
+	attr.mq_msgsize = sizeof(int);
+	attr.mq_flags   = 0;		/* a blocking queue  */
 	
-	//TODO: Send num to the pdes.
-
-
 
 
 	printf("Creating Producers.\n");
@@ -111,7 +95,8 @@ int main(int argc, char *argv[])
 			doProducerWork();
 			break;
 		}
-		printf("Created Producer with PID %d and ID %d.\n", PID, i);
+		producerArray[i] = pid;
+		//printf("Created Producer with PID %d and ID %d.\n", pid, i);
 	}
 	
 	if(pid!=0){
@@ -127,11 +112,29 @@ int main(int argc, char *argv[])
 				doConsumerWork();
 				break;
 			}
-			printf("Created Consumer with PID %d and ID %d.\n", PID, i);
+			consumerArray[i] = pid;
+			//printf("Created Consumer with PID %d and ID %d.\n", pid, i);
 		}
 	}
 	
-	
+	if(pid!=0){
+		printf("Waitin for cleanup\n");
+		for(int i =0; i < (num_c); i++){
+			int tmp=0;
+			waitpid(consumerArray[i], &tmp, 0); 
+		}
+		for(int i =0; i < (num_p); i++){
+			int tmp=0;
+			waitpid(producerArray[i], &tmp, 0); 
+		}
+		if (mq_close(qdes) == -1) {
+			perror("mq_close() failed");
+			exit(2);
+		}
+		
+		
+		printf("Cleaned up\n");
+	}
 	
 	exit(0);
 }
@@ -149,12 +152,46 @@ void printTime(){
 }
 
 void doProducerWork(){
-	int produce = -1;
 	
-	//while the num in pdes is not 0 keep creating items
+	for(int i =0; i< num; i++){
+		int total_work;
+		if(i%num_p == producer_id){
+			total_work = -1;
+
+			//send your i to the queue;
+			if (mq_send(qdes, (char *)&i , sizeof(int), 0) == -1) {
+				printf("mq_send() failed\n");
+			}
+			//printf("Sent work %d from producer %d\n", i, producer_id);
+		}
+	}
+	if (mq_close(qdes) == -1) {
+		perror("mq_close() failed");
+		exit(2);
+	}
+	exit(0);
 }
 void doConsumerWork(){
-	//check the qdes for items
+	
+	for(int i =0; i<num; i++){
+			//receive value from buffer
+			int work = -1;
+			struct timespec ts = {time(0) + 1, 0};
+			if (mq_timedreceive(qdes, (char *) &work, sizeof(int), 0, &ts) == -1) {
+				//If you time out then there are no more values left
+				// if (mq_close(qdes) == -1) {
+				// 	exit(2);
+				// }
+				// exit(0);
+			}
 
+			int temp = sqrt(work);
+			if(temp*temp == work && work !=-1){
+				printf("CID: %d WORK: %d SQRT: %d \n", consumer_id, work, temp);
+			}
+			if(work == num){
+				exit(0);
+			}
+	}
 }
 
