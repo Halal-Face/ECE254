180d0e92f4a3a738fd9bfa23f8be3df4f1e46f7a
Zahin Mohammad <Zahin.Mohammad@gmail.com>
2018-11-28 16:33:17 -0500

    Modularize

--- lab4/starter/mem.c
+++ lab4/starter/mem.c
@@ -274,34 +274,35 @@ void dealloc(void *ptr, int setting)
 /* count how many free blocks are less than the input size */ 
 int best_fit_count_extfrag(size_t size)
 {
-	// To be completed by students
-    node *temp_node = bfm_head;
-    int count = 0;
-
-    while(temp_node != NULL){
-        if((temp_node->allocated == FALSE) && (temp_node->free_mem < size)){
-
-            count++;
-        }
-        temp_node = temp_node->next;
-		
-    }
-	return count;
+	  return (count_extfrag(size, BEST_FIT));
 }
 
 int worst_fit_count_extfrag(size_t size)
 {
 	// To be completed by students
-    node *temp_node = wfm_head;
-    int count = 0;
-    
-    while(temp_node != NULL){
+    return (count_extfrag(size, WORST_FIT));
+}
+
+int count_extfrag(size_t size, int setting)
+{
+	node *temp_node;
+	int count = 0;
+	if(setting == BEST_FIT)
+	{
+		temp_node = bfm_head;
+	}
+	else if(setting == WORST_FIT)
+	{
+		temp_node = wfm_head;
+	}
+	while(temp_node != NULL){
         if((temp_node->allocated == FALSE) && (temp_node->free_mem < size)){
             count++;
         }
         temp_node = temp_node->next;
     }
 	return count;
+
 }
 
 //Print out the blocks information
