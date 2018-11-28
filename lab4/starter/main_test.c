0e99d0e1a686435a186b3b0592d9ddac28c03a29
minghao li <minghaoli@minghaos-MacBook-Pro.local>
2018-11-28 18:33:33 -0500

    Created the lab report

--- lab4/starter/main_test.c
+++ lab4/starter/main_test.c
@@ -26,35 +26,68 @@ void print_ll(int setting);
 
 int main(int argc, char *argv[])
 {
-	int num = 0;
-	srand ( time(NULL) );
+    srand ( time(NULL) );
+    if(argc == 2 && atoi(argv[1]) == 1)
+    {
+        // BEST FIT TEST
+        void* ptr[30];
+        best_fit_memory_init(1024);    // initizae 1KB, best fit
+        printf("Testing Best Fit: \n");
+        int i = randomAlloc(BEST_FIT, ptr);
+        printf("Best Fit after Alloc loop:\n");
+        print_ll(BEST_FIT);
+        randomDeAlloc(BEST_FIT, ptr, i);
+        printf("Best Fit after DeAlloc loop, should Dealloc all odd Nodes:\n");
+        print_ll(BEST_FIT);
+        randomDeAlloc(BEST_FIT, ptr, i);
+        printf("Best Fit after DeAlloc loop, should Dealloc all even Nodes:\n");
+        print_ll(BEST_FIT);
+        
+        // WORST FIT TEST
+        void* ptr2[30];
+        worst_fit_memory_init(1024);    // initizae 1KB, best fit
+        printf("Testing Worst Fit: \n");
+        i = randomAlloc(WORST_FIT, ptr2);
+        printf("Worst Fit after Alloc loop:\n");
+        print_ll(WORST_FIT);
+        randomDeAlloc(WORST_FIT, ptr2, i);
+        printf("Worst Fit after DeAlloc loop, should Dealloc all odd Nodes:\n");
+        print_ll(WORST_FIT);
+        randomDeAlloc(WORST_FIT, ptr2, i);
+        printf("WORST Fit after DeAlloc loop, should Dealloc all even Nodes:\n");
+        print_ll(WORST_FIT);
+        return 0;
+    }
+    else{
+        int num = 0;
 
-	void* ptr[30];
-	printf("BEST FIT,");
-	best_fit_memory_init(1024);	// initizae 1KB, best fit
-	for(int i=0; i<101; i++)
-	{
-		randomDeAlloc(BEST_FIT, ptr, randomAlloc(BEST_FIT, ptr));
-		num = best_fit_count_extfrag(5);
-		printf("%d,", num);
-	}
-	printf("\n");
-		
+        void* ptr[30];
+        printf("BEST FIT,");
+        best_fit_memory_init(1024);	// initizae 1KB, best fit
+        for(int i=0; i<101; i++)
+        {
+            randomDeAlloc(BEST_FIT, ptr, randomAlloc(BEST_FIT, ptr));
+            num = best_fit_count_extfrag(5);
+            printf("%d,", num);
+        }
+        printf("\n");
+        
 
-	
-	void* ptr2[30];
-	printf("WORSTT FIT,");
-	worst_fit_memory_init(1024);	// initizae 1KB, best fit
-	for(int i=0; i<101; i++)
-	{
-		randomDeAlloc(WORST_FIT, ptr2, randomAlloc(WORST_FIT, ptr2));
-		num = worst_fit_count_extfrag(5);
-		printf("%d,", num);
-	}
-	printf("\n");
-	
+        
+        void* ptr2[30];
+        printf("WORSTT FIT,");
+        worst_fit_memory_init(1024);	// initizae 1KB, best fit
+        for(int i=0; i<101; i++)
+        {
+            randomDeAlloc(WORST_FIT, ptr2, randomAlloc(WORST_FIT, ptr2));
+            num = worst_fit_count_extfrag(5);
+            printf("%d,", num);
+        }
+        printf("\n");
+        
 
-	return 0;
+        return 0;
+    }
 }
 
 int randomAlloc(int setting, void* ptr[])
